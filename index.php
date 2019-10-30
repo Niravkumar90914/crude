d91abbade39d5cb44bf2ed98d95f1eeb908a8bf6
Niravkumar90914 <57092849+Niravkumar90914@users.noreply.github.com>
2019-10-30 08:37:18 +0530

    commit

--- index.php
+++ index.php
@@ -3,54 +3,69 @@
 
 <head>
 	<title>Data Table</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-	<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
 	<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.css" />
-
-
 	<script type="text/javascript" src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.js"></script>
-
-
-
-
 	<style type="text/css">
 		.error {
 			color: red;
 		}
+
+		thead {
+			//background-color:steelblue;
+		}
+
+		.link-head {}
 	</style>
 </head>
 
 <body>
-	<div class="container-fluid">
-		<div class="jumbotron p-2">
-			<h2>Student data</h2>
+	<div class="container-fluid ">
+		<div class="jumbotron p-2 m-2 bg-info ">
+			<h2 class="text-white"> Student data</h2>
 		</div>
-		<div class="row">
-			<div class="col-sm-2">bgdfgdg</div>
+		<div class="row w-100" >
+			<div class="col-sm-2">
+
+				<ul class="list-group m-1 ml-2">
+					<li class="list-group-item bg-dark text-white">Links</li>
+					<li class="list-group-item bg-secondary">Add data</li>
+					<li class="list-group-item bg-secondary">Image gallery</li>
+				</ul>
+
+			</div>
 			<div class="col-sm-10">
-				<table id="student-list" class="table table-bordered  table-responsive ">
-					<thead class="bg-secondary">
+				<div class="card m-1 bg-gray">
+					<div class="text-right p-1"><button class="btn btn-primary w-25 ">Add data</button></div>
+				</div>
+				<table id="student-list" class="table  table-responsive table-bordered">
+					<thead class="bg-dark">
 						<tr class="text-white">
 							<th>Name</th>
+							<th>Photo</th>
 							<th>Gender</th>
-							<th>Date of birth</th>
+							<th> Date of birth</th>
 							<th>Email address</th>
 							<th>Mobile number</th>
 							<th>State</th>
 							<th>City</th>
 							<th>Address</th>
 							<th>Hobbies</th>
-							<th>Photo</th>
 							<th>Date</th>
 						</tr>
 					</thead>
 					<tbody></tbody>
 					<tfoot></tfoot>
 				</table>
+
+
 			</div>
 		</div>
 	</div>
@@ -59,24 +74,38 @@
 	$(document).ready(function() {
 		$("#student-list").dataTable({
 			responsive: true,
+			autoWidth:false,
 			ajax: {
 				url: 'student.php',
 				data: {
 					"action": "display_student_data"
 				},
-				dataSrc: ''
+				"dataSrc": function(json) {
+
+					for (var i = 0, ien = json.length; i < ien; i++) {
+
+						json[i].student_image_name = '<img width="100%" height="80px" src="assets/img/' + json[i].student_image_name + '">';
+					}
+					return json;
+
+				}
 			},
-			columns: [{
-					data: "student_name"
+			columns: [
+
+				{
+					data: "student_name", "width":"150px"
 				},
 				{
-					data: "gender"
+					data: "student_image_name","width":"100px"
 				},
 				{
-					data: "date_of_birth"
+					data: "gender", "width":"30px"
 				},
 				{
-					data: "email_address"
+					data: "date_of_birth", "width":"70px"					
+				},
+				{
+					data: "email_address",
 				},
 				{
 					data: "mobile_number"
@@ -91,10 +120,7 @@
 					data: "student_address"
 				},
 				{
-					data: "student_hobbies"
-				},
-				{
-					data: "student_image_name"
+					data: "student_hobbies", width:"80px"
 				},
 				{
 					data: "registration_date"
@@ -103,7 +129,7 @@
 			"columnDefs": [{
 					"orderable": false,
 					"targets": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-				},
+				}
 
 			]
 
